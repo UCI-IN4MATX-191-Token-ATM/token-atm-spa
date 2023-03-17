d94307ecee6d99daa4bf50ed4195cacb4db13709
Yongkang Man <yongkanm@uci.edu>
2023-03-16 17:13:24 -0700

    Add NgBootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +8,7 @@ import { AppComponent } from './app.component';
 @NgModule({
     declarations: [AppComponent],
     imports: [BrowserModule, AppRoutingModule],
-    providers: [],
+    providers: [NgbModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
