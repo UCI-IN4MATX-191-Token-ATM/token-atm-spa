e63657df16338f037d317dfd3d8d6b6e83dbec1e
Yongkang Man <yongkanm@uci.edu>
2023-04-12 23:55:27 -0700

    Change routing to add course selection after providing credentials & Pass selected course as state to DashboardComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourseSelectionComponent } from './components/course-selection/course-selection.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'dashboard', component: DashboardComponent },
+    { path: 'select-course', component: CourseSelectionComponent },
     { path: '', pathMatch: 'full', redirectTo: '/login' }
 ];
 
