ab1add9197466c2a03bbc561df0a5f5d6589c8e6
Yongkang Man <yongkanm@uci.edu>
2023-05-11 16:46:46 -0700

    UI Mockup

--- src/app/components/token-option-configuration/token-option-configuration.component.ts
+++ src/app/components/token-option-configuration/token-option-configuration.component.ts
@@ -1,6 +1,19 @@
 import { Component } from '@angular/core';
 import type { Course } from 'app/data/course';
 import type { CourseConfigurable } from '../dashboard/dashboard-routing';
+import { v4 as uuidv4 } from 'uuid';
+
+type ITokenOption = {
+    name: string;
+    type: string;
+    balanceChange: number;
+};
+
+type ITokenOptionGroup = {
+    name: string;
+    uuid: string;
+    options: ITokenOption[];
+};
 
 @Component({
     selector: 'app-token-option-configuration',
@@ -10,8 +23,55 @@ import type { CourseConfigurable } from '../dashboard/dashboard-routing';
 export class TokenOptionConfigurationComponent implements CourseConfigurable {
     course?: Course;
 
+    private static DATA: ITokenOptionGroup[] = [
+        {
+            name: 'Module 1',
+            uuid: uuidv4(),
+            options: []
+        },
+        {
+            name: 'Module 2',
+            uuid: uuidv4(),
+            options: [
+                {
+                    name: 'Quiz #1',
+                    type: 'Canvas Quiz',
+                    balanceChange: 2
+                },
+                {
+                    name: 'Feedback Survey Participation',
+                    type: 'Qualtrics Survey',
+                    balanceChange: 1
+                },
+                {
+                    name: 'Lab 1 Data Set',
+                    type: 'Lab Data',
+                    balanceChange: -2
+                },
+                {
+                    name: 'Lab Report 1 Resubmission',
+                    type: 'Assignment Resubmission',
+                    balanceChange: -1
+                }
+            ]
+        },
+        {
+            name: 'Module 3',
+            uuid: uuidv4(),
+            options: []
+        }
+    ];
+
     async configureCourse(course: Course): Promise<void> {
         // TODO: configure course for token option configuration
         this.course = course;
+    }
+
+    get allData() {
+        return TokenOptionConfigurationComponent.DATA;
+    }
+
+    getAbsValue(value: number) {
+        return Math.abs(value);
     }
 }
