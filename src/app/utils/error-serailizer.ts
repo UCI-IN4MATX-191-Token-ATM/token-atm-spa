d82dbe85169524f53803d07a4c6c2a6b5ad80a88
epsilonError <99434682+epsilonError@users.noreply.github.com>
2023-06-30 00:15:46 +0000

    Reformat Error Serializer text

--- src/app/utils/error-serailizer.ts
+++ src/app/utils/error-serailizer.ts
@@ -3,8 +3,8 @@ import { stringify } from 'flatted';
 export class ErrorSerializer {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     public static serailize(err: any): string {
-        let result = 'toString() result: ' + err.toString() + '\n';
-        result += 'JSON result: ' + stringify(err);
+        let result = '\ntoString() result: \n' + err.toString();
+        result += '\nJSON result: \n' + stringify(err);
         return result;
     }
 }
