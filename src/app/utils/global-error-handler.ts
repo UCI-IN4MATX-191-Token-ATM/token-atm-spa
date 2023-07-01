15d789ae070458c2e791b9f364eb5544cc54bfa3
epsilonError <99434682+epsilonError@users.noreply.github.com>
2023-07-01 10:51:43 +0000

    Update CSS and add spacing

--- src/app/utils/global-error-handler.ts
+++ src/app/utils/global-error-handler.ts
@@ -11,7 +11,8 @@ export class GlobalErrorHandler implements ErrorHandler {
         }
         win.document.open();
         win.document.write(
-            `<head><title>Error Report</title></head><body>An Error occured. Sorry for the inconvenience. <br/>Error Message: <br/> ${ErrorSerializer.serailize(
+            `<head><title>Error Report</title></head>
+            <body style="white-space: pre-line; overflow-wrap: anywhere;">An Error occured. Sorry for the inconvenience. <br/>______________<br/>Error Message: ${ErrorSerializer.serailize(
                 error
             )}</body>`
         );
