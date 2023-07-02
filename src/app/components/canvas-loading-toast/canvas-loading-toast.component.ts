348a83698ddc6bac9947603b9f94048a77f93d34
Yongkang Man <yongkanm@uci.edu>
2023-07-02 16:48:34 +0000

    Remove the use of ng-bootstrap

---
+++ src/app/components/canvas-loading-toast/canvas-loading-toast.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-canvas-loading-toast',
+    templateUrl: './canvas-loading-toast.component.html',
+    styleUrls: ['./canvas-loading-toast.component.sass']
+})
+export class CanvasLoadingToastComponent {}
