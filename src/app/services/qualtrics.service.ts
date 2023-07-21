15b29647062ecdf620136912069cc22e88854b5a
epsilonError <99434682+epsilonError@users.noreply.github.com>
2023-07-21 03:28:53 +0000

    Specify scopes

--- src/app/services/qualtrics.service.ts
+++ src/app/services/qualtrics.service.ts
@@ -36,7 +36,7 @@ export class QualtricsService {
         } catch (err: unknown) {
             return err;
         }
-        // TODO: Check if the required scopes are enabled
+        // TODO: Check if the required scopes are enabled (read:users and read:survey_responses)
         return undefined;
     }
 
