404f218ff25325877e9ff6ed12564fd16a0cd3ea
epsilonError <99434682+epsilonError@users.noreply.github.com>
2023-08-26 06:16:07 +0000

    Collect errors, improve validation, and add todos

--- src/app/components/batch-token-balance-adjustment-modal/batch-token-balance-adjustment-modal.component.ts
+++ src/app/components/batch-token-balance-adjustment-modal/batch-token-balance-adjustment-modal.component.ts
@@ -6,6 +6,7 @@ import { StudentRecordManagerService } from 'app/services/student-record-manager
 import { countAndNoun } from 'app/utils/pluralize';
 import type { BsModalRef } from 'ngx-bootstrap/modal';
 import * as CSVParse from 'papaparse';
+import { AbstractControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-batch-token-balance-adjustment-modal',
@@ -21,6 +22,8 @@ export class BatchTokenBalanceAdjustmentModalComponent {
     progress?: string;
     progressMessage?: string;
 
+    errorCollection?: string[][];
+
     constructor(
         @Inject(StudentRecordManagerService) private managerService: StudentRecordManagerService,
         @Inject(CanvasService) private canvasService: CanvasService
@@ -30,6 +33,18 @@ export class BatchTokenBalanceAdjustmentModalComponent {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         this.selectedFile = (event.target as any)?.files[0];
     }
+
+    // TODO: - Selectable Columns
+    //       - Header Parsing
+    //       - Parsing from & Unparsing to Excel formatted CSVs (https://www.papaparse.com/faq#encoding)
+    //       - Downloadable CSV blob for rows with Token ATM Errors
+    //          - Use file `.name` and append error label + time
+    //          - Use file `.path` for suggested download directory
+    //       - Expose Parser Errors & Meta info (`results.errors` & `results.meta`)
+    //          - Allow dry-run functionality to test CSV without updating Token ATM
+    //       - Make/Use a type for parsed row data (string[] or JSON[])
+    //       - Update parser to use a stream to parse row by row
+    //       - Only preserve columns needed by Token ATM in Error CSV
 
     async onImportCSV() {
         if (!this.selectedFile || !this.configuration) return;
@@ -45,36 +60,55 @@ export class BatchTokenBalanceAdjustmentModalComponent {
         });
         const result = await promise;
         let cnt = 0;
+        this.errorCollection = new Array<string[]>();
         for (const data of result) {
             cnt++;
             this.progress = (((cnt - 1) / result.length) * 100).toFixed(2);
             this.progressMessage = `${cnt - 1} out of ${countAndNoun(result.length, 'record')} processed`;
-            if (!Array.isArray(data) || (data.length != 2 && data.length != 3)) continue;
-            const email = data[0];
-            if (typeof email != 'string') continue;
-            const tokenBalanceChange = parseFloat(data[1] as string);
-            if (isNaN(tokenBalanceChange)) continue;
-            const message = data[2];
-            const student = await this.canvasService.getStudentByEmail(this.configuration.course.id, email);
-            if (!student) continue;
-            const studentRecord = await this.managerService.getStudentRecord(this.configuration, student);
-            const nowTime = new Date();
-            await this.managerService.logProcessedRequest(
-                this.configuration,
-                studentRecord,
-                new ProcessedRequest(
+            try {
+                if (!Array.isArray(data) || (data.length != 2 && data.length != 3)) {
+                    if (data.length === 1 && data[0]?.trim() === '') continue; // Skip empty and whitespace rows
+                    throw new Error('Not enough CSV columns');
+                }
+                const email = data[0];
+                if (typeof email != 'string' || Validators.email({ value: email } as AbstractControl) != null)
+                    throw new Error('Email isn’t a valid email');
+                const tokenBalanceChange = parseFloat(data[1] as string);
+                if (isNaN(tokenBalanceChange)) throw new Error('Token Balance Change isn’t a number');
+                const message = data[2];
+                const student = await this.canvasService.getStudentByEmail(this.configuration.course.id, email);
+                if (!student) throw new Error('No Student found with this email');
+                const studentRecord = await this.managerService.getStudentRecord(this.configuration, student);
+                const nowTime = new Date();
+                await this.managerService.logProcessedRequest(
                     this.configuration,
-                    -1,
-                    'Manual Adjustment',
-                    studentRecord.student,
-                    true,
-                    nowTime,
-                    nowTime,
-                    tokenBalanceChange,
-                    message ?? ''
-                )
-            );
+                    studentRecord,
+                    new ProcessedRequest(
+                        this.configuration,
+                        -1,
+                        'Manual Adjustment',
+                        studentRecord.student,
+                        true,
+                        nowTime,
+                        nowTime,
+                        tokenBalanceChange,
+                        message ?? ''
+                    )
+                );
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            } catch (err: any) {
+                this.collectError(data, cnt, err.toString());
+                console.log(this.errorCollection);
+            }
         }
+        console.log('Error CSV Text:', CSVParse.unparse(this.errorCollection));
         this.modalRef?.hide();
+    }
+
+    collectError(record: string[], line: number, errorMessage: string) {
+        if (this.errorCollection == null) {
+            throw new Error('Error collector has not been initialized');
+        }
+        this.errorCollection.push([...record, `line ${line}`, errorMessage.replaceAll(/[\n\r]/g, '  ')]);
     }
 }
