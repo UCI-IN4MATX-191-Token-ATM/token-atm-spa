cb568464424a70300a8a8b4b360ba44c9dbf1952
epsilonError <99434682+epsilonError@users.noreply.github.com>
2023-10-11 16:11:03 +0000

    Init service

---
+++ src/app/services/csvs.service.spec.ts
@@ -0,0 +1,16 @@
+// import { TestBed } from '@angular/core/testing';
+
+// import { CSVsService } from './csvs.service';
+
+// describe('CSVsService', () => {
+//   let service: CSVsService;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(CSVsService);
+//   });
+
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+// });
