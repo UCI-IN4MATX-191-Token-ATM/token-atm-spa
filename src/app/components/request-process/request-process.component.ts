38f625c738f687b7265b7a463406f535758fd775
epsilonError <99434682+epsilonError@users.noreply.github.com>
2023-12-04 19:18:04 +0000

    Make modal handle async calls properly

--- src/app/components/request-process/request-process.component.ts
+++ src/app/components/request-process/request-process.component.ts
@@ -42,18 +42,7 @@ export class RequestProcessComponent implements CourseConfigurable {
     public async onStartRequestProcessing(): Promise<void> {
         if (!this.configuration) return;
         if (!(await this.configurationManager.isTokenATMLogPublished(this.configuration))) {
-            const confirmPublish = await this.modalManagerService.createConfirmationModalWithoutRef(
-                'The Token ATM Log assignment on Canvas must be published to process student requests.\n\nWould you like Token ATM to publish this assignment for you?',
-                'Publish Token ATM Log?',
-                false,
-                'I’ll publish it myself.',
-                'Yes, publish it for me.'
-            );
-            if (confirmPublish) {
-                await this.configurationManager.publishTokenATMLog(this.configuration);
-            } else {
-                return;
-            }
+            if (!(await this.onPublishLog())) return;
         }
         this.isReconfigureFinished = false;
         this.isStopRequested = false;
@@ -99,5 +88,24 @@ export class RequestProcessComponent implements CourseConfigurable {
             (this.requestProcessManagerService.isRunning && this.isStopRequested) ||
             (!this.requestProcessManagerService.isRunning && !this.isReconfigureFinished)
         );
+    }
+
+    public async onPublishLog(): Promise<boolean> {
+        if (!this.configuration) return false;
+        const [confirmationRef, result] = await this.modalManagerService.createConfirmationModal(
+            'The Token ATM Log assignment on Canvas must be published to process student requests.\n\nWould you like Token ATM to publish this assignment for you?',
+            'Publish Token ATM Log?',
+            false,
+            'I’ll publish it myself.',
+            'Yes, publish it for me.'
+        );
+        if (result) {
+            await this.configurationManager.publishTokenATMLog(this.configuration);
+            confirmationRef.hide();
+            return true;
+        } else {
+            confirmationRef.hide();
+            return false;
+        }
     }
 }
