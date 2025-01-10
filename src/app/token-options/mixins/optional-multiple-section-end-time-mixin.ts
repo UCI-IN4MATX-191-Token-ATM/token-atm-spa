1478a0e1e8f3ba239dabd7389a162869b3029dac
epsilonError <99434682+epsilonError@users.noreply.github.com>
2025-01-10 22:30:16 +0000

    Make empty defaults in Config into optional props
    `startTime` and `endTime` have a default/unused/unset/empty value of
    `null`.
    
    The Multiple Section Start/End Time setting for these mixins are
    optional. Currently the UI has a checkbox that when clicked reveals the
    forms to collect Single and/or Multiple Section info.
    
    This commit changes the constructed Optional Types to include whether or
    not the the forms are used/viewable.
    
    The UI form's viewability (or user disinterest in the optional config)
    is represented by the property being Optional (either absent or set as
    undefined). This stands in for the unchecked boolean nature of the
    checkbox.
    
    Once viewable (or user has expressed interest) then the form's default
    state is shown.
    
    The functionality added here is a backwards compatible way to make the
    `startTime` and `endTime` optional properties in the stored Token ATM
    Configuration.
    
    The goal is reduce the stored JSON length since undefined props are
    stripped when stringified. This is a useful change for possibly adding
    `startTime` and `endTime` configuration to the baseline Token Option
    Mixin. Which means they would be included as a choice in all Token
    Options.
    
    Adding a required `startTime: null` & `endTime: null` to every Token
    Option would needlessly increase the length/size of the Token ATM
    Configuration.
    
    The backwards compatible change adds:
    - acceptance of undefined/absent `startTime` or `endTime` in the Token
    ATM Configuration,
    - converts the optionals into their default `null` property for use at
    runtime, and
    - changes `null`s into `undefined` when writing them back to the
    Token ATM Configuration. This will strip `null`s from existing Token
    Option configurations when they are next saved.
    
    The stripping of `null`s will break the Token ATM Configuration's
    compatiblity with earlier versions of Token ATM. So while this change is
    backwards compatible, it is not downgrade compatible.

--- src/app/token-options/mixins/optional-multiple-section-end-time-mixin.ts
+++ src/app/token-options/mixins/optional-multiple-section-end-time-mixin.ts
@@ -1,11 +1,34 @@
 import * as t from 'io-ts';
+import { chain } from 'fp-ts/Either';
 import { type Constructor, DateDef } from 'app/utils/mixin-helper';
 import type { IGridViewDataSource } from './grid-view-data-source-mixin';
 import { MultipleSectionDateMatcher, MultipleSectionDateMatcherDef } from 'app/utils/multiple-section-date-matcher';
 
-export const OptionalMultipleSectionEndTimeMixinDataDef = t.strict({
+const MultipleSectionEndTimeMixinDataDef = t.strict({
     endTime: t.union([DateDef, MultipleSectionDateMatcherDef, t.null])
 });
+
+type MultipleSectionEndTimeMixinData = t.TypeOf<typeof MultipleSectionEndTimeMixinDataDef>;
+const PartialMultipleSectionEndTimeMixinDataDef = t.exact(t.partial(MultipleSectionEndTimeMixinDataDef.type.props));
+type PartialMultipleSectionEndTimeMixinData = t.TypeOf<typeof PartialMultipleSectionEndTimeMixinDataDef>;
+
+// TODO: Make Utility for constructing this kind of `io-ts` Type
+// (see also `optional-multiple-section-start-time-mixin.ts`)
+export const OptionalMultipleSectionEndTimeMixinDataDef = new t.Type<
+    MultipleSectionEndTimeMixinData,
+    PartialMultipleSectionEndTimeMixinData,
+    unknown
+>(
+    'OptionalMultipleSectionEndTimeMixinDataDef',
+    MultipleSectionEndTimeMixinDataDef.is,
+    (v, ctx) =>
+        chain((v: PartialMultipleSectionEndTimeMixinData): t.Validation<MultipleSectionEndTimeMixinData> => {
+            return v.endTime === undefined
+                ? t.success({ endTime: null })
+                : MultipleSectionEndTimeMixinDataDef.validate(v, ctx);
+        })(PartialMultipleSectionEndTimeMixinDataDef.validate(v, ctx)),
+    (v) => (v.endTime === null ? { endTime: undefined } : { endTime: v.endTime })
+);
 
 export type OptionalMultipleSectionEndTimeMixinData = t.TypeOf<typeof OptionalMultipleSectionEndTimeMixinDataDef>;
 export type RawOptionalMultipleSectionEndTimeMixinData = t.OutputOf<typeof OptionalMultipleSectionEndTimeMixinDataDef>;
