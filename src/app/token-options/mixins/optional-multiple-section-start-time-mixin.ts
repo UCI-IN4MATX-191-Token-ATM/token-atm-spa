c1e7125ce492e867a7ed9db625c132f611bd7854
epsilonError <99434682+epsilonError@users.noreply.github.com>
2025-01-23 00:10:38 +0000

    Comment out Optional/Partial implementation

--- src/app/token-options/mixins/optional-multiple-section-start-time-mixin.ts
+++ src/app/token-options/mixins/optional-multiple-section-start-time-mixin.ts
@@ -1,33 +1,33 @@
 import * as t from 'io-ts';
-import { chain } from 'fp-ts/Either';
+// import { chain } from 'fp-ts/Either';
 import { type Constructor, DateDef } from 'app/utils/mixin-helper';
 import type { IGridViewDataSource } from './grid-view-data-source-mixin';
 import { MultipleSectionDateMatcher, MultipleSectionDateMatcherDef } from 'app/utils/multiple-section-date-matcher';
 
-const MultipleSectionStartTimeMixinDataDef = t.strict({
+export const OptionalMultipleSectionStartTimeMixinDataDef = t.strict({
     startTime: t.union([DateDef, MultipleSectionDateMatcherDef, t.null])
 });
-type MultipleSectionStartTimeMixinData = t.TypeOf<typeof MultipleSectionStartTimeMixinDataDef>;
-const PartialMultipleSectionStartTimeMixinDataDef = t.exact(t.partial(MultipleSectionStartTimeMixinDataDef.type.props));
-type PartialMultipleSectionStartTimeMixinData = t.TypeOf<typeof PartialMultipleSectionStartTimeMixinDataDef>;
+// type MultipleSectionStartTimeMixinData = t.TypeOf<typeof MultipleSectionStartTimeMixinDataDef>;
+// const PartialMultipleSectionStartTimeMixinDataDef = t.exact(t.partial(MultipleSectionStartTimeMixinDataDef.type.props));
+// type PartialMultipleSectionStartTimeMixinData = t.TypeOf<typeof PartialMultipleSectionStartTimeMixinDataDef>;
 
-// TODO: Make Utility for constructing this kind of `io-ts` Type
-// (see also `optional-multiple-section-end-time-mixin.ts`)
-export const OptionalMultipleSectionStartTimeMixinDataDef = new t.Type<
-    MultipleSectionStartTimeMixinData,
-    PartialMultipleSectionStartTimeMixinData,
-    unknown
->(
-    'OptionalMultipleSectionStartTimeMixinDataDef',
-    MultipleSectionStartTimeMixinDataDef.is,
-    (v, ctx) =>
-        chain((v: PartialMultipleSectionStartTimeMixinData): t.Validation<MultipleSectionStartTimeMixinData> => {
-            return v.startTime === undefined
-                ? t.success({ startTime: null })
-                : MultipleSectionStartTimeMixinDataDef.validate(v, ctx);
-        })(PartialMultipleSectionStartTimeMixinDataDef.validate(v, ctx)),
-    (v) => (v.startTime === null ? { startTime: undefined } : { startTime: v.startTime })
-);
+// // TODO: Make Utility for constructing this kind of `io-ts` Type
+// // (see also `optional-multiple-section-end-time-mixin.ts`)
+// export const OptionalMultipleSectionStartTimeMixinDataDef = new t.Type<
+//     MultipleSectionStartTimeMixinData,
+//     PartialMultipleSectionStartTimeMixinData,
+//     unknown
+// >(
+//     'OptionalMultipleSectionStartTimeMixinDataDef',
+//     MultipleSectionStartTimeMixinDataDef.is,
+//     (v, ctx) =>
+//         chain((v: PartialMultipleSectionStartTimeMixinData): t.Validation<MultipleSectionStartTimeMixinData> => {
+//             return v.startTime === undefined
+//                 ? t.success({ startTime: null })
+//                 : MultipleSectionStartTimeMixinDataDef.validate(v, ctx);
+//         })(PartialMultipleSectionStartTimeMixinDataDef.validate(v, ctx)),
+//     (v) => (v.startTime === null ? { startTime: undefined } : { startTime: v.startTime })
+// );
 
 export type OptionalMultipleSectionStartTimeMixinData = t.TypeOf<typeof OptionalMultipleSectionStartTimeMixinDataDef>;
 export type RawOptionalMultipleSectionStartTimeMixinData = t.OutputOf<
